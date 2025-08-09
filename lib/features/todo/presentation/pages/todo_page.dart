a9acf2bd63bf81128777680171e7514d7e840407
majid <majid.gha3mi.p@gmail.com>
2025-08-09 15:15:38 +0330

    created todo_page

---
+++ lib/features/todo/presentation/pages/todo_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controllers/todo_controller.dart';
+
+class TodoPage extends StatelessWidget {
+  final TextEditingController controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final todoCtrl = Get.find<TodoController>();
+
+    return Scaffold(
+      appBar: AppBar(title: Text("My ToDos")),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(controller: controller),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                if (controller.text.isNotEmpty) {
+                  todoCtrl.addTodo(controller.text);
+                  controller.clear();
+                }
+              },
+              child: Text("Add Todo")),
+          Expanded(
+            child: Obx(() => ListView.builder(
+              itemCount: todoCtrl.todos.length,
+              itemBuilder: (context, index) {
+                final todo = todoCtrl.todos[index];
+                return ListTile(title: Text(todo.title));
+              },
+            )),
+          ),
+        ],
+      ),
+    );
+  }
+}
