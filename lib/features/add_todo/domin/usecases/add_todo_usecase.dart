8a21c984d2b4cd46b4b68487d53dadbd76215b58
majid <majid.gha3mi.p@gmail.com>
2025-08-10 12:34:57 +0330

    created AddTodoUseCasea add lib/features/add_todo/domin/usecases/

---
+++ lib/features/add_todo/domin/usecases/add_todo_usecase.dart
@@ -0,0 +1,9 @@
+import 'package:todo_clean/features/add_todo/domin/repositories/add_todo_repository.dart';
+import '../entities/todo_entity.dart';
+
+class AddTodoUseCase {
+  final AddTodoRepository repository;
+  AddTodoUseCase(this.repository);
+
+  Future<void> call(TodoEntity todo) => repository.addTodo(todo);
+}
