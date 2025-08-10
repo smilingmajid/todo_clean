88809c8b3eab2bf620ba1551922e0a612b4e0654
majid <majid.gha3mi.p@gmail.com>
2025-08-10 12:45:56 +0330

    created AddTodoUseCasea add lib/features/add_todo/data/models/todo_model.dart

---
+++ lib/features/add_todo/data/models/todo_model.dart
@@ -0,0 +1,33 @@
+import 'package:hive/hive.dart';
+
+import '../../domin/entities/todo_entity.dart';
+
+
+part 'todo_model.g.dart';
+
+@HiveType(typeId: 0)
+class TodoModel extends TodoEntity {
+  @override
+  @HiveField(0)
+  // ignore: overridden_fields
+  final String id;
+  @override
+  @HiveField(1)
+  // ignore: overridden_fields
+  final String title;
+  @override
+  @HiveField(2)
+  // ignore: overridden_fields
+  final bool isDone;
+
+  TodoModel({
+    required this.id,
+    required this.title,
+    this.isDone = false,
+  }) : super(id: id, title: title, isDone: isDone);
+
+  factory TodoModel.fromEntity(TodoEntity entity) =>
+      TodoModel(id: entity.id, title: entity.title, isDone: entity.isDone);
+
+  TodoEntity toEntity() => TodoEntity(id: id, title: title, isDone: isDone);
+}
