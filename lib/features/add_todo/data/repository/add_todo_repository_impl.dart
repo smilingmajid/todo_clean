8033130c97a72aaf17f06b5d3df7f49ed7cdc459
majid <majid.gha3mi.p@gmail.com>
2025-08-10 13:08:14 +0330

    edit add_todo_repository_impl

--- lib/features/add_todo/data/repository/add_todo_repository_impl.dart
+++ lib/features/add_todo/data/repository/add_todo_repository_impl.dart
@@ -1,3 +1,16 @@
-class AddTodoRepositoryImpl {
+import '../../domin/entities/todo_entity.dart';
+import '../../domin/repositories/add_todo_repository.dart';
+import '../datasource/todo_local_datasource.dart';
+import '../models/todo_model.dart';
 
+class AddTodoRepositoryImpl implements AddTodoRepository {
+  final TodoLocalDataSource local;
+
+  AddTodoRepositoryImpl(this.local);
+
+  @override
+  Future<void> addTodo(TodoEntity todo) {
+    final model = TodoModel.fromEntity(todo);
+    return local.addTodoModel(model);
+  }
 }
