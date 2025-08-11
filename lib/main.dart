136b89b591de7357c200ebd35cc1a176e5151724
majid <majid.gha3mi.p@gmail.com>
2025-08-11 14:57:30 +0330

    editing main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'features/todo/presentation/bindings/todo_binding.dart';
-import 'features/todo/presentation/pages/todo_page.dart';
-import 'features/todo/data/models/todo_model.dart';
+import 'package:todo_clean/di/feature_binding.dart';
+import 'features/add_todo/data/models/todo_model.dart';
+import 'features/show_todo/presentation/pages/show_todo_page.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,18 +12,10 @@ void main() async {
   Hive.registerAdapter(TodoModelAdapter());
   await Hive.openBox<TodoModel>('todos');
 
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialBinding: TodoBinding(),
-      home: TodoPage(),
-    );
-  }
+  runApp(
+    GetMaterialApp(
+      initialBinding: FeatureBinding(),
+      home: ShowTodoPage(),
+    ),
+  );
 }
