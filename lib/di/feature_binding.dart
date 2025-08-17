e173fcdf4f2c97813c0582ff9621be6aeadd19f5
majid <majid.gha3mi.p@gmail.com>
2025-08-17 11:29:31 +0330

    erore on feature_binding fixed

--- lib/di/feature_binding.dart
+++ lib/di/feature_binding.dart
@@ -1,14 +1,15 @@
 import 'package:get/get.dart';
 import 'package:hive/hive.dart';
-//import '../features/add_todo/data/datasources/todo_local_datasource.dart' as add_ds;
+
+import '../features/add_todo/data/datasource/todo_local_datasource.dart' as add_ds;
 import '../features/add_todo/data/models/todo_model.dart';
 
 import '../features/add_todo/data/repository/add_todo_repository_impl.dart';
-
 import '../features/add_todo/domin/usecases/add_todo_usecase.dart';
 import '../features/add_todo/presentation/controllers/add_todo_controller.dart';
+
+
 import '../features/show_todo/data/datasources/todo_local_datasource.dart' as show_ds;
-import '../features/show_todo/data/datasources/todo_local_datasource.dart' as add_ds;
 import '../features/show_todo/data/repositories/show_todo_repository_impl.dart';
 import '../features/show_todo/domin/usecases/get_todos_usecase.dart';
 import '../features/show_todo/presentation/controllers/show_todo_controller.dart';
@@ -18,10 +19,17 @@ class FeatureBinding extends Bindings {
   void dependencies() {
     final box = Hive.box<TodoModel>('todos');
 
-    final addLocal = add_ds.TodoLocalDataSourceImpl(box);
+    final addLocal = add_ds.TodoLocalDataSourceImpl(box);   
     final showLocal = show_ds.TodoLocalDataSourceImpl(box);
 
-    Get.lazyPut(() => AddTodoController(AddTodoUseCase(AddTodoRepositoryImpl(addLocal))));
-    Get.lazyPut(() => ShowTodoController(GetTodosUseCase(ShowTodoRepositoryImpl(showLocal))));
+    Get.lazyPut(
+      () => AddTodoController(AddTodoUseCase(AddTodoRepositoryImpl(addLocal))),
+    );
+
+    Get.lazyPut(
+      () => ShowTodoController(
+        GetTodosUseCase(ShowTodoRepositoryImpl(showLocal)),
+      ),
+    );
   }
 }
--- lib/features/add_todo/data/datasource/todo_local_datasource.dart
+++ lib/features/add_todo/data/datasource/todo_local_datasource.dart
@@ -1,5 +1,17 @@
+import 'package:hive/hive.dart';
 import '../models/todo_model.dart';
 
 abstract class TodoLocalDataSource {
   Future<void> addTodoModel(TodoModel todo);
+}
+
+class TodoLocalDataSourceImpl implements TodoLocalDataSource {
+  final Box<TodoModel> box;
+
+  TodoLocalDataSourceImpl(this.box);
+
+  @override
+  Future<void> addTodoModel(TodoModel todo) async {
+    await box.put(todo.id, todo);
+  }
 }
