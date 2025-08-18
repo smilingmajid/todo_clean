328df44d4d3279f6d1a512b83b6de5a127ceedc8
majid <majid.gha3mi.p@gmail.com>
2025-08-18 14:59:55 +0330

    scaffold background color set dark mode

--- lib/features/home/presentation/page/home_page.dart
+++ lib/features/home/presentation/page/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo_clean/controller/theme_controller.dart';
+import 'package:todo_clean/core/theme/app_color.dart';
     
 class HomePage extends StatelessWidget {
 
@@ -6,11 +9,14 @@ class HomePage extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+    bool isDark=Get.find<ThemeController>().isDark.value;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('home screen'),
-      ),
-      body: Container(),
+     backgroundColor: isDark?AppColor().darkModeColors[0]:AppColor().lightModeColors[0],
+      body: Column(
+        children: [
+          const Text('home screen'),
+        ],
+      )
     );
   }
 }
