efa21a106885a19c7ee6f10f9d5e1af91724818c
majid <majid.gha3mi.p@gmail.com>
2025-08-18 14:53:27 +0330

    main.dart refactor to darkmode and light ode

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,24 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todo_clean/di/feature_binding.dart';
 import 'package:todo_clean/features/home/presentation/page/home_page.dart';
 import 'features/add_todo/data/models/todo_model.dart';
+import 'core/theme/theme_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   Hive.registerAdapter(TodoModelAdapter());
   await Hive.openBox<TodoModel>('todos');
+  await Hive.openBox('themeBox');
+  final themeService = ThemeService();
 
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: await themeService.loadTheme() ? ThemeMode.dark : ThemeMode.light,
       initialBinding: FeatureBinding(),
       home: HomePage(),
-     
     ),
   );
 }
