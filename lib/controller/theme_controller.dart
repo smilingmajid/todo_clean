4bef661874b680daa10c20ffc1d0b97fddd72b32
majid <majid.gha3mi.p@gmail.com>
2025-08-18 14:21:01 +0330

    saving data on hive with theme service add to theme controller

--- lib/controller/theme_controller.dart
+++ lib/controller/theme_controller.dart
@@ -1,9 +1,21 @@
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+
+import '../core/theme/theme_service.dart';
 
 class ThemeController extends GetxController {
-  RxBool isDark = true.obs;
+  var isDark = false.obs;
+  final ThemeService _themeService = ThemeService();
 
-  void changeTheme(isDark) {
-    isDark.value = !isDark.value;
+  @override
+  void onInit() async {
+    super.onInit();
+    isDark.value = await _themeService.loadTheme();
+    Get.changeThemeMode(isDark.value ? ThemeMode.dark : ThemeMode.light);
+  }
+
+  void changeTheme() async {
+    await _themeService.switchTheme();
+    isDark.value = await _themeService.loadTheme();
   }
 }
