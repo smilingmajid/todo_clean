e0e1221e962aa23adfbf8f7cb00b98c09a867b21
majid <majid.gha3mi.p@gmail.com>
2025-08-18 13:53:44 +0330

    set HomePage add to home on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todo_clean/di/feature_binding.dart';
+import 'package:todo_clean/features/home/presentation/page/home_page.dart';
 import 'features/add_todo/data/models/todo_model.dart';
-import 'features/show_todo/presentation/pages/show_todo_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,7 +15,8 @@ void main() async {
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
       initialBinding: FeatureBinding(),
-      home: ShowTodoPage(),
+      home: HomePage(),
+     
     ),
   );
 }
