ee6859bcc1b2e216edf0b80f094cd58bfdeae84c
majid <majid.gha3mi.p@gmail.com>
2025-08-19 18:27:20 +0330

    created en translations

---
+++ lib/core/translations/en_us.dart
@@ -0,0 +1,32 @@
+ final Map<String, String> enUS = {
+  'HomeScreen.AppBar': 'Home Screen',
+  'HomeScreen.NoProject': 'No projects yet.\nTap + to add a new project.',
+
+  'TaskScreen.AddTask': 'Add New Task ...',
+  'TaskScreen.ActiveTask': 'In Progress Tasks',
+  'TaskScreen.CompletedTasks': 'Completed Tasks',
+
+  'DrawerWidget.GitHub': 'GitHub',
+  'DrawerWidget.Instagram': 'Instagram',
+  'DrawerWidget.LinkedIn': 'LinkedIn',
+  'DrawerWidget.Telegram': 'Telegram',
+  'DrawerWidget.YouTube': 'YouTube',
+  'DrawerWidget.Name': 'Majid Ghasemy',
+  'DrawerWidget.Job': 'Mid Level flutter Developer',
+
+  'OnboardingScreen.GetXTitle': 'GetX',
+  'OnboardingScreen.GetXBody': 'Used for state management, routing and dependencies.',
+  'OnboardingScreen.HiveTitle': 'Hive',
+  'OnboardingScreen.HiveBody': 'Local data storage powered by Hive.',
+  'OnboardingScreen.MVCTitle': 'Flutter MVC',
+  'OnboardingScreen.MVCBody': 'The app follows a Model-View-Controller architecture.',
+  'OnboardingScreen.StartTitle': 'Get Started',
+  'OnboardingScreen.StartBody': 'Ready to manage your tasks?',
+  'OnboardingScreen.StartButton': 'Start Now',
+  'OnboardingScreen.Skip': 'Skip',
+  'OnboardingScreen.Done': 'Done',
+
+  'Project.addProject': 'Add New Project',
+  'Project.add': 'Add',
+  'Project.cancel': 'Cancel',
+};
