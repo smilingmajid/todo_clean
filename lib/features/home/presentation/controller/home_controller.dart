1fe8c19de17eb4dcd66e71cc7f7f1b90b0cded8a
majid <majid.gha3mi.p@gmail.com>
2025-08-19 16:23:04 +0330

    home controller created

---
+++ lib/features/home/presentation/controller/home_controller.dart
@@ -0,0 +1,29 @@
+import 'package:get/get.dart';
+
+import '../../../project/data/models/project_model.dart';
+import '../../../project/domin/usecase/add_project_usecase.dart';
+
+class HomeController extends GetxController {
+  final AddProjectUseCase addProjectUseCase;
+  HomeController(this.addProjectUseCase);
+
+  var projects = <ProjectModel>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    loadProjects();
+  }
+
+  void addProject(String name) async {
+    final project = ProjectModel(name);
+    await addProjectUseCase(project);
+    projects.add(project);
+  }
+
+  void loadProjects() async {
+    projects.clear();
+    final list = await addProjectUseCase.repository.getProjects();
+    projects.addAll(list);
+  }
+}
