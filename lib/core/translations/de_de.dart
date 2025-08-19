1c1d32cd8a5d7ae7ebc50ca7e989d7083391f936
majid <majid.gha3mi.p@gmail.com>
2025-08-19 18:29:43 +0330

    created de translations

---
+++ lib/core/translations/de_de.dart
@@ -0,0 +1,32 @@
+final Map<String, String> deDE = {
+  'HomeScreen.AppBar': 'Home Screen',
+  'HomeScreen.NoProject': 'Noch keine Projekte vorhanden.\nTippe auf +, um ein neues Projekt hinzuzufügen',
+
+  'TaskScreen.AddTask': 'Neue Aufgabe hinzufügen ...',
+  'TaskScreen.ActiveTask': 'Laufende Aufgaben',
+  'TaskScreen.CompletedTasks': 'Abgeschlossene Aufgaben',
+
+  'DrawerWidget.GitHub': 'GitHub',
+  'DrawerWidget.Instagram': 'Instagram',
+  'DrawerWidget.Lindin': 'Linkedin',
+  'DrawerWidget.Telegram': 'Telegram',
+  'DrawerWidget.YouTube': 'YouTube',
+  'DrawerWidget.Name': 'Majid Ghasemy',
+  'DrawerWidget.Job': 'Mid Level flutter Developer',
+
+  'OnboardingScreen.GetXTitle': 'GetX',
+  'OnboardingScreen.GetXBody': 'Wird für Zustandsverwaltung, Routing und Abhängigkeiten verwendet.',
+  'OnboardingScreen.HiveTitle': 'Hive',
+  'OnboardingScreen.HiveBody': 'Lokale Datenspeicherung mit Hive.',
+  'OnboardingScreen.MVCTitle': 'Flutter MVC',
+  'OnboardingScreen.MVCBody': 'Die App verwendet das MVC-Architekturmuster.',
+  'OnboardingScreen.StartTitle': 'Los geht\'s',
+  'OnboardingScreen.StartBody': 'Bereit, deine Aufgaben zu verwalten?',
+  'OnboardingScreen.StartButton': 'Jetzt starten',
+  'OnboardingScreen.Skip': 'Überspringen',
+  'OnboardingScreen.Done': 'Fertig',
+
+  'Project.addProject': 'Neues Projekt hinzufügen',
+  'Project.add': 'Hinzufügen',
+  'Project.cancel': 'Abbrechen',
+};
