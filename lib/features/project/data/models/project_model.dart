1c77949bef679ab5778b84fefc15600b02c4a848
majid <majid.gha3mi.p@gmail.com>
2025-08-19 14:39:56 +0330

    project model created

---
+++ lib/features/project/data/models/project_model.dart
@@ -0,0 +1,11 @@
+import 'package:hive/hive.dart';
+
+part 'project_model.g.dart';
+
+@HiveType(typeId: 1)
+class ProjectModel extends HiveObject {
+  @HiveField(0)
+  final String name;
+
+  ProjectModel(this.name);
+}
