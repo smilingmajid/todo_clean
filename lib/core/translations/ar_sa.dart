c5c6602b82f8958404950494188855b594645e83
majid <majid.gha3mi.p@gmail.com>
2025-08-19 18:30:27 +0330

    created ar translations

---
+++ lib/core/translations/ar_sa.dart
@@ -0,0 +1,32 @@
+final Map<String, String> arSA = {
+  'HomeScreen.AppBar': 'الرئيسية',
+  'HomeScreen.NoProject': 'لا توجد مشاريع بعد.\nانقر على + لإضافة مشروع جديد.',
+
+  'TaskScreen.AddTask': 'أضف مهمة جديدة ...',
+  'TaskScreen.ActiveTask': 'المهام الجارية',
+  'TaskScreen.CompletedTasks': 'المهام المكتملة',
+
+  'DrawerWidget.GitHub': 'جيت هاب',
+  'DrawerWidget.Instagram': 'إنستغرام',
+  'DrawerWidget.Lindin': 'لينكد إن',
+  'DrawerWidget.Telegram': 'تيليجرام',
+  'DrawerWidget.YouTube': 'يوتيوب',
+  'DrawerWidget.Name': 'مجيد قاسمي',
+  'DrawerWidget.Job': 'مطور فلاتر',
+
+  'OnboardingScreen.GetXTitle': 'GetX',
+  'OnboardingScreen.GetXBody': 'يستخدم لإدارة الحالة والتوجيه والاعتماديات.',
+  'OnboardingScreen.HiveTitle': 'Hive',
+  'OnboardingScreen.HiveBody': 'يتم تخزين البيانات محليًا باستخدام Hive.',
+  'OnboardingScreen.MVCTitle': 'Flutter MVC',
+  'OnboardingScreen.MVCBody': 'تتبع التطبيق نمط نموذج-عرض-تحكم (MVC).',
+  'OnboardingScreen.StartTitle': 'ابدأ',
+  'OnboardingScreen.StartBody': 'هل أنت مستعد لإدارة مهامك؟',
+  'OnboardingScreen.StartButton': 'ابدأ الآن',
+  'OnboardingScreen.Skip': 'تخطي',
+  'OnboardingScreen.Done': 'تم',
+
+  'Project.addProject': 'إضافة مشروع جديد',
+  'Project.add': 'إضافة',
+  'Project.cancel': 'إلغاء',
+};
