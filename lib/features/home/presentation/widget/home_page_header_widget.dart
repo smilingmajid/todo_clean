19fd568bef36736e240a207425e9577098310de4
majid <majid.gha3mi.p@gmail.com>
2025-08-19 12:11:59 +0330

    iconsax add to header

--- lib/features/home/presentation/widget/home_page_header_widget.dart
+++ lib/features/home/presentation/widget/home_page_header_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:random_avatar/random_avatar.dart';
 
 Widget homePageHeaderWidget(bool isDark, {Function()? onPressed}) {
