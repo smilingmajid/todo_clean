45a466fd192a4162db490e5560d7ce65e70b26ef
majid <majid.gha3mi.p@gmail.com>
2025-08-19 18:28:37 +0330

    created fa translations

---
+++ lib/core/translations/fa_ir.dart
@@ -0,0 +1,32 @@
+final Map<String, String> faIR = {
+  'HomeScreen.AppBar': 'صفحه اصلی',
+  'HomeScreen.NoProject': 'هنوز هیچ پروژه‌ای وجود ندارد.\nبرای افزودن پروژه جدید، روی + بزنید.',
+
+  'TaskScreen.AddTask': 'افزودن وظیفه جدید ...',
+  'TaskScreen.ActiveTask': 'وظایف در حال انجام',
+  'TaskScreen.CompletedTasks': 'وظایف تکمیل‌شده',
+
+  'DrawerWidget.GitHub': 'گیت هاب',
+  'DrawerWidget.Instagram': 'اینستاگرام',
+  'DrawerWidget.Lindin': 'لینکدین',
+  'DrawerWidget.Telegram': 'تلگرام',
+  'DrawerWidget.YouTube': 'یوتیوب',
+  'DrawerWidget.Name': 'مجید قاسمی',
+  'DrawerWidget.Job': 'توسعه دهنده فلاتر',
+
+  'OnboardingScreen.GetXTitle': 'GetX',
+  'OnboardingScreen.GetXBody': 'برای مدیریت وضعیت، مسیر‌یابی و وابستگی‌ها استفاده شده.',
+  'OnboardingScreen.HiveTitle': 'Hive',
+  'OnboardingScreen.HiveBody': 'برای ذخیره‌سازی داده‌های محلی از Hive استفاده شده است.',
+  'OnboardingScreen.MVCTitle': 'Flutter MVC',
+  'OnboardingScreen.MVCBody': 'برنامه از معماری MVC (مدل-نما-کنترلر) استفاده می‌کند.',
+  'OnboardingScreen.StartTitle': 'شروع کن',
+  'OnboardingScreen.StartBody': 'آماده‌ای وظایف‌ت رو مدیریت کنی؟',
+  'OnboardingScreen.StartButton': 'شروع',
+  'OnboardingScreen.Skip': 'رد کردن',
+  'OnboardingScreen.Done': 'اتمام',
+
+  'Project.addProject': 'افزودن پروژه جدید',
+  'Project.add': 'افزودن',
+  'Project.cancel': 'لغو',
+};
