b34480dafb18c55b05deb3732ddbc3453c9d5be5
majid <majid.gha3mi.p@gmail.com>
2025-08-19 16:26:58 +0330

    add and show project add to home page

--- lib/features/home/presentation/page/home_page.dart
+++ lib/features/home/presentation/page/home_page.dart
@@ -1,34 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:todo_clean/controller/theme_controller.dart';
-import 'package:todo_clean/core/theme/app_color.dart';
-import '../widget/home_page_header_widget.dart';
+
+import '../../../../core/theme/app_color.dart';
+import '../controller/home_controller.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final themeController = Get.find<ThemeController>();
+    final controller = Get.find<HomeController>();
 
     return Obx(() {
-      final isDark = themeController.isDark.value;
-      return SafeArea(
-        child: Scaffold(
-          backgroundColor:
-              isDark
-                  ? AppColor().darkModeColors[0]
-                  : AppColor().lightModeColors[0],
-          body: Column(
-            children: [
-              homePageHeaderWidget(
-                isDark,
-                onPressed: () {
-                  themeController.changeTheme();
-                },
-              ),
-            ],
-          ),
+      return Scaffold(
+        backgroundColor: AppColor().lightModeColors[0],
+        body: ListView.builder(
+          itemCount: controller.projects.length,
+          itemBuilder: (context, index) {
+            return ListTile(title: Text(controller.projects[index].name));
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            String name = '';
+            showDialog(
+              context: context,
+              builder:
+                  (_) => AlertDialog(
+                    title: Text('New Project'),
+                    content: TextField(onChanged: (value) => name = value),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          if (name.isNotEmpty) controller.addProject(name);
+                          Get.back();
+                        },
+                        child: Text('Add'),
+                      ),
+                    ],
+                  ),
+            );
+          },
+          child: Icon(Icons.add),
         ),
       );
     });
