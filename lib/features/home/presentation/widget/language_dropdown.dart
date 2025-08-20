d3ca3e0a97c62ded0c694f52e06c977523aa0d5c
majid <majid.gha3mi.p@gmail.com>
2025-08-20 19:03:47 +0330

    language drop down update

--- lib/features/home/presentation/widget/language_dropdown.dart
+++ lib/features/home/presentation/widget/language_dropdown.dart
@@ -1,35 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '../../../../core/translations/language_controller.dart';
 
+import '../../../../core/translations/language_controller.dart';
 
 class LanguageDropdown extends StatelessWidget {
   const LanguageDropdown({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final LanguageController controller = Get.find<LanguageController>();
+    final controller = Get.find<LanguageController>();
 
-    return Obx(() {
-      return DropdownButton<Map<String, Object>>(
-        value: controller.selectedLang,
+    return Obx(
+      () => DropdownButton<Map<String, dynamic>>(
+        // ignore: invalid_use_of_protected_member
+        value: controller.selectedLang.value,
         underline: const SizedBox(),
-        items: controller.languages.map((lang) {
-          return DropdownMenuItem<Map<String, Object>>(
-            value: lang,
-            child: Row(
-              children: [
-                Image.asset(lang['flag'] as String, width: 24, height: 24),
-                const SizedBox(width: 8),
-                Text(lang['name'] as String),
-              ],
-            ),
-          );
-        }).toList(),
+        items:
+            controller.languages.map((lang) {
+              return DropdownMenuItem(
+                value: lang,
+                child: Row(
+                  children: [
+                    Image.asset(lang['flag'], width: 24, height: 24),
+                    const SizedBox(width: 8),
+                    Text(lang['name']),
+                  ],
+                ),
+              );
+            }).toList(),
         onChanged: (value) {
           if (value != null) controller.changeLanguage(value);
         },
-      );
-    });
+      ),
+    );
   }
 }
