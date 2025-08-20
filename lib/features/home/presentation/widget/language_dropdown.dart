757b217e65b450b66d3af0d690ac5f867563ca30
majid <majid.gha3mi.p@gmail.com>
2025-08-20 21:42:13 +0330

    language drop down update base language model and write text_widget

--- lib/features/home/presentation/widget/language_dropdown.dart
+++ lib/features/home/presentation/widget/language_dropdown.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:todo_clean/core/widgets/text_widget.dart';
 
+import '../../../../controller/theme_controller.dart';
+import '../../../../core/models/language_model.dart';
 import '../../../../core/translations/language_controller.dart';
 
 class LanguageDropdown extends StatelessWidget {
@@ -9,27 +12,41 @@ class LanguageDropdown extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = Get.find<LanguageController>();
+    bool isDark = Get.find<ThemeController>().isDark.value;
 
     return Obx(
-      () => DropdownButton<Map<String, dynamic>>(
-        // ignore: invalid_use_of_protected_member
+      () => DropdownButton<Language>(
         value: controller.selectedLang.value,
         underline: const SizedBox(),
         items:
             controller.languages.map((lang) {
-              return DropdownMenuItem(
+              return DropdownMenuItem<Language>(
                 value: lang,
-                child: Row(
-                  children: [
-                    Image.asset(lang['flag'], width: 24, height: 24),
-                    const SizedBox(width: 8),
-                    Text(lang['name']),
-                  ],
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    right:
+                        (lang.locale.languageCode == 'fa' ||
+                                lang.locale.languageCode == 'ar')
+                            ? 8
+                            : 0,
+                    left:
+                        (lang.locale.languageCode == 'en' ||
+                                lang.locale.languageCode == 'de')
+                            ? 8
+                            : 0,
+                  ),
+                  child: Row(
+                    children: [
+                      Image.asset(lang.flag, width: 24, height: 24),
+                      const SizedBox(width: 20),
+                      textWidget(isDark: isDark, txt: lang.name, fontSize: 15),
+                    ],
+                  ),
                 ),
               );
             }).toList(),
-        onChanged: (value) {
-          if (value != null) controller.changeLanguage(value);
+        onChanged: (lang) {
+          if (lang != null) controller.changeLanguage(lang);
         },
       ),
     );
