a36e154546f3977c3eeffd05dd0a2167d34acf55
majid <majid.gha3mi.p@gmail.com>
2025-08-21 12:34:54 +0330

    HiveTranslateRepository update

--- lib/features/translation/data/datasource/hive_translate_datasource.dart
+++ lib/features/translation/data/datasource/hive_translate_datasource.dart
@@ -1,6 +1,9 @@
 import 'package:hive/hive.dart';
+
 import 'package:flutter/material.dart';
-import 'package:todo_clean/features/translation/domin/entities/language.dart';
+
+
+import '../../domin/entities/language.dart';
 import '../../domin/repositories/translate_repository.dart';
 
 class HiveTranslateRepository implements TranslateRepository {
@@ -8,40 +11,27 @@ class HiveTranslateRepository implements TranslateRepository {
   static const String keyLangCode = "language_code";
   static const String keyCountryCode = "country_code";
 
+  late Box box;
+
   final List<Language> supportedLanguages = [
-    Language(
-      name: 'English',
-      locale: const Locale('en', 'US'),
-      flag: 'assets/image/us.png',
-    ),
-    Language(
-      name: 'Deutsch',
-      locale: const Locale('de', 'DE'),
-      flag: 'assets/image/de.png',
-    ),
-    Language(
-      name: 'فارسی',
-      locale: const Locale('fa', 'IR'),
-      flag: 'assets/image/ir.png',
-    ),
-    Language(
-      name: 'العربية',
-      locale: const Locale('ar', 'SA'),
-      flag: 'assets/image/sa.png',
-    ),
+    Language(name: 'English', locale: const Locale('en', 'US'), flag: 'assets/images/us.png'),
+    Language(name: 'Deutsch', locale: const Locale('de', 'DE'), flag: 'assets/images/de.png'),
+    Language(name: 'فارسی', locale: const Locale('fa', 'IR'), flag: 'assets/images/ir.png'),
+    Language(name: 'العربية', locale: const Locale('ar', 'SA'), flag: 'assets/images/sa.png'),
   ];
+
+  Future<void> init() async {
+    box = await Hive.openBox(boxName);
+  }
 
   @override
   Future<Language> getCurrentLanguage() async {
-    var box = await Hive.openBox(boxName);
-    String? langCode = box.get(keyLangCode);
-    String? countryCode = box.get(keyCountryCode);
+    final langCode = box.get(keyLangCode);
+    final countryCode = box.get(keyCountryCode);
 
     if (langCode != null && countryCode != null) {
       return supportedLanguages.firstWhere(
-        (lang) =>
-            lang.locale.languageCode == langCode &&
-            lang.locale.countryCode == countryCode,
+        (lang) => lang.locale.languageCode == langCode && lang.locale.countryCode == countryCode,
         orElse: () => supportedLanguages.first,
       );
     }
@@ -50,8 +40,8 @@ class HiveTranslateRepository implements TranslateRepository {
 
   @override
   Future<void> changeLanguage(Language language) async {
-    var box = await Hive.openBox(boxName);
     await box.put(keyLangCode, language.locale.languageCode);
     await box.put(keyCountryCode, language.locale.countryCode);
+    await box.flush();
   }
 }
