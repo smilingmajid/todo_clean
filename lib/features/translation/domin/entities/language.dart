4ae54f877bf4523977793999afcf58313823176d
majid <majid.gha3mi.p@gmail.com>
2025-08-21 11:42:52 +0330

    translation feature created

---
+++ lib/features/translation/domin/entities/language.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Language {
+  final String name;
+  final Locale locale;
+  final String flag;
+
+  Language({required this.name, required this.locale, required this.flag});
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Language &&
+          runtimeType == other.runtimeType &&
+          locale == other.locale;
+
+  @override
+  int get hashCode => locale.hashCode;
+}
