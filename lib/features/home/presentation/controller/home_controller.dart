4c71908c4e67b6d557ba8a4ee853679a1a072bdc
majid <majid.gha3mi.p@gmail.com>
2025-08-22 21:06:24 +0330

    ProjectColor add to addProject methd on Homecontroller

--- lib/features/home/presentation/controller/home_controller.dart
+++ lib/features/home/presentation/controller/home_controller.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:todo_clean/core/theme/project_colors.dart';
 import '../../../project/data/models/project_model.dart';
 import '../../../project/domin/usecase/add_project_usecase.dart';
 
@@ -14,8 +15,16 @@ class HomeController extends GetxController {
     loadProjects();
   }
 
-  void addProject(String name) async {
-    final project = ProjectModel(name,);
+  void addProject(
+    String name, //, int count
+  ) async {
+    
+    int count = projects.length;
+    final project = ProjectModel(
+      name: name,
+      // ignore: deprecated_member_use
+      colorValue: ProjectColors.palette[count].value,
+    );
     await addProjectUseCase(project);
     projects.add(project);
   }
