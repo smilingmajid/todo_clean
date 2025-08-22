8e3e71490db973f43ac92001909032179b2b32a4
majid <majid.gha3mi.p@gmail.com>
2025-08-22 13:20:33 +0330

    language_dropdown set font size 15, padding 8

--- lib/features/translation/presentation/widgets/language_dropdown_widget.dart
+++ lib/features/translation/presentation/widgets/language_dropdown_widget.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:todo_clean/controller/theme_controller.dart';
@@ -7,30 +6,39 @@ import 'package:todo_clean/core/widgets/text_widget.dart';
 import '../../domin/entities/language.dart';
 import '../controller/translate_controller.dart';
 
-class LanguageDropdownWidget  extends StatelessWidget {
-  const LanguageDropdownWidget ({super.key});
+class LanguageDropdownWidget extends StatelessWidget {
+  const LanguageDropdownWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     final controller = Get.find<TranslateController>();
-bool isDark=Get.find<ThemeController>().isDark.value;
+    bool isDark = Get.find<ThemeController>().isDark.value;
 
     return Obx(
       () => DropdownButton<Language>(
         value: controller.selectedLang.value,
         underline: const SizedBox(),
-        items: controller.languages.map((lang) {
-          return DropdownMenuItem<Language>(
-            value: lang,
-            child: Row(
-              children: [
-                Image.asset(lang.flag, width: 24, height: 24),
-                const SizedBox(width: 8),
-                textWidget(isDark:isDark,txt:lang.name),
-              ],
-            ),
-          );
-        }).toList(),
+        items:
+            controller.languages.map((lang) {
+              return DropdownMenuItem<Language>(
+                value: lang,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Image.asset(lang.flag, width: 24, height: 24),
+                      const SizedBox(width: 8),
+                      textWidget(
+                        isDark: isDark,
+                        txt: lang.name,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }).toList(),
         onChanged: (lang) {
           if (lang != null) controller.changeLanguage(lang);
         },
